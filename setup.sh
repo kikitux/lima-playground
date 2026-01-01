b8527f8d799662e4d00a66e782e68eec6c2a3e02
kikitux <kikitux@mainframe.kikitux.net>
2026-01-01 18:09:34 +0100

    added OS logic to setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,29 @@
+#!/bin/env bash
+
+set -e
 
 echo checking if lima is installed
-brew list lima lima-additional-guestagents &>/dev/null || {
-  brew update
-  brew upgrade
-  echo installing lima
-  brew install lima lima-additional-guestagents
+
+command -v lima &>/dev/null && {
+  echo lima is already installed
+  exit 0
 }
+
+OS=$(uname)
+
+# for macos
+if [[ $OS == "Darwin" ]]; then
+  brew list lima lima-additional-guestagents &>/dev/null || {
+    brew update
+    brew upgrade
+    echo installing lima
+    brew install lima lima-additional-guestagents
+  }
+elif [[ $OS == "Linux" ]]; then
+  
+  VERSION=$(curl -fsSL https://api.github.com/repos/lima-vm/lima/releases/latest | jq -r .tag_name)
+  curl -fsSL "https://github.com/lima-vm/lima/releases/download/${VERSION}/lima-${VERSION:1}-$(uname -s)-$(uname -m).tar.gz" | sudo tar Cxzvm /usr/local
+  curl -fsSL "https://github.com/lima-vm/lima/releases/download/${VERSION}/lima-additional-guestagents-${VERSION:1}-$(uname -s)-$(uname -m).tar.gz" | sudo tar Cxzvm /usr/local
+
+fi
 

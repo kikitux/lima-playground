2e7bc037fff176a5667630ff2a9713d6a91c654b
Alvaro Miranda Aguilera <kikitux+github@gmail.com>
2026-01-05 08:42:01 +0100

    Modify VS Code tunnel script for architecture-specific download
    Update download URL for VS Code tunnel script based on architecture.

--- vscode_tunnel.sh
+++ vscode_tunnel.sh
@@ -1,7 +1,18 @@
+#!/usr/bin/env bash
+
+# this install and un VS Code tunnel
+
+OS=$(uname)
+ARCH=$(uname -m)
+case "$ARCH" in
+  x86_64) ARCH=x64 ;;
+  aarch64|arm64) ARCH=arm64 ;;
+  *) echo "Unsupported arch: $ARCH"; exit 1 ;;
+esac
+
 [ -f ./code ] || {
-  curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-arm64' --output vscode_cli.tar.gz
+  curl -Lk "https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-${ARCH}" --output vscode_cli.tar.gz
   tar -xf vscode_cli.tar.gz
 }
 
-export KUBECONFIG=/var/lib/microshift/resources/kubeadmin/kubeconfig
 ./code tunnel
